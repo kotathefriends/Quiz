f1c9a58f79bb3ef5327320a1d47119a4f7a87c1c
kotathefriends <5070kouta@gmail.com>
2023-10-19 22:00:07 +0900

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by KOTA TAKAHASHI on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
